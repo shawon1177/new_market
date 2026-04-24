fff983c57132b1f7f111db01b6cb151141d02ef2
RS SEFAT <sefathasan4@gmail.com>
2026-04-24 23:51:50 +0600

    accounts model updated

--- accounts/models.py
+++ accounts/models.py
@@ -4,10 +4,10 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 class User(AbstractUser):
     email = models.EmailField(unique=True, db_index=True)
-    phone = models.CharField(max_length=20, blank=True)
+    password = models.CharField(max_length=128)
 
     USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = ["username"]
+    REQUIRED_FIELDS = ["username", "password"]
 
     def __str__(self):
         return self.email
