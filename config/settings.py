a7abd7d67a8612fbe74bb8994364494266761827
RS SEFAT <sefathasan4@gmail.com>
2026-05-04 22:43:23 +0600

    fix cloudinary config and deployment settings

--- config/settings.py
+++ config/settings.py
@@ -88,9 +88,9 @@ MIDDLEWARE = [
 ROOT_URLCONF = 'config.urls'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 CLOUDINARY_STORAGE = {
-    'CLOUD_NAME': config('dob1z35ro'),
-    'API_KEY': config('191844463265151'),
-    'API_SECRET': config('lde2uEz4sVcHqYkNNkxJajZEO4w'),
+    'CLOUD_NAME': config('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY': config('CLOUDINARY_API_KEY'),
+    'API_SECRET': config('CLOUDINARY_API_SECRET'),
 }
 TEMPLATES = [
     {
